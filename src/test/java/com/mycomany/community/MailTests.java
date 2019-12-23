f6541869f80d6b23fa4e205272dd6c06cf3fd6fe
HuangXin <huangxin2525074@gmail.com>
2019-12-23 21:21:55 +0800

    commit test

---
+++ src/test/java/com/mycomany/community/MailTests.java
@@ -0,0 +1,41 @@
+package com.mycomany.community;
+
+import com.mycomany.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+   @Autowired
+    private MailClient mailClient;
+
+   @Autowired
+   private TemplateEngine templateEgine;
+
+   @Test
+    public void testTextMail(){
+       mailClient.sendMail("huangxin2525074@gmail.com","Test","Welcome!");
+   }
+
+   @Test
+    public void testHtmlEmail(){
+       Context context = new Context();
+       context.setVariable("username","sunday");
+
+      String contents = templateEgine.process("/mail/demo",context);
+      System.out.println(contents);
+
+       mailClient.sendMail("huangxin2525074@sina.com","html",contents);
+   }
+
+
+}
